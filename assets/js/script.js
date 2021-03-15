7a71aec1b78a6fe23bb6afc2d8d4a258934c019d
ShawonBarman <shawonbarmon34@gmail.com>
2021-03-15 11:21:31 +0600

    1st commit after add git

---
+++ assets/js/script.js
@@ -0,0 +1,117 @@
+$(function() {
+    $(window).load(function(){
+        $("body").addClass("loaded")
+    }),
+
+    $('.navbar-toggle').click(function() {
+        $(this).toggleClass('act');
+            if($(this).hasClass('act')) {
+                $('.main-menu').addClass('act');
+            }
+            else {
+                $('.main-menu').removeClass('act');
+            }
+    });
+
+    //jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(document).on('click', '.page-scroll a', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.site-header',
+        offset: 10
+    });
+
+	/* Progress bar */
+    function tdProgress(container){
+	
+	"use strict";
+		
+	container.find('.progress_inner').each(function() {
+		var progress 		= jQuery(this);
+		var pValue 			= parseInt(progress.data('value'), 10);
+		var pColor			= progress.data('color');
+		var pBarWrap 		= progress.find('.bar');
+		var pBar 			= progress.find('.bar_in');
+		pBar.css({width:pValue+'%', backgroundColor:pColor});
+		setTimeout(function(){pBarWrap.addClass('open');});
+	});
+}
+
+	jQuery('.arlo_progress').each(function() {
+
+		"use strict";
+
+		var pWrap 			= jQuery(this);
+		pWrap.waypoint({handler: function(){tdProgress(pWrap);},offset:'90%'});	
+	});
+
+    /* Counters  */
+    if ($(".section-counters .start").length>0) {
+        $(".section-counters .start").each(function() {
+            var stat_item = $(this),
+            offset = stat_item.offset().top;
+            $(window).scroll(function() {
+                if($(window).scrollTop() > (offset - 1000) && !(stat_item.hasClass('counting'))) {
+                    stat_item.addClass('counting');
+                    stat_item.countTo();
+                }
+            });
+        });
+    };
+
+	// another custom callback for counting to infinity
+	$('#infinity').data('countToOptions', {
+		onComplete: function (value) {
+		  count.call(this, {
+		    from: value,
+		    to: value + 1
+		  });
+		}
+	});
+
+	$('#infinity').each(count);
+
+	function count(options) {
+        var $this = $(this);
+        options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+        $this.countTo(options);
+    }
+
+    // Navigation overlay
+    var s = skrollr.init({
+            forceHeight: false,
+            smoothScrolling: false,
+            mobileDeceleration: 0.004,
+            mobileCheck: function() {
+                //hack - forces mobile version to be off
+                return false;
+            }
+    });
+    
+});
+jQuery(document).ready(function(){
+
+	"use strict";
+	arlo_tm_animate_text();
+});
+// animation text word
+function arlo_tm_animate_text(){
+
+    "use strict";
+
+    var animateSpan			= jQuery('.arlo_tm_animation_text_word');
+
+        animateSpan.typed({
+            strings: ["Web Designer.", "Full Stack Web Developer.", "Graphic Designer."],
+            loop: true,
+            startDelay: 1e3,
+            backDelay: 2e3
+        });
+}
